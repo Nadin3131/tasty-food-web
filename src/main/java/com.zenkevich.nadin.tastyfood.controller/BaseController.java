a42d72df06b4a3e5ac287daf074b1684449ae380
Nadin <ladutk.nadin@gmail.com>
2016-12-07 23:05:34 +0300

    Added logger

--- src/main/java/com.zenkevich.nadin.tastyfood.controller/BaseController.java
+++ src/main/java/com.zenkevich.nadin.tastyfood.controller/BaseController.java
@@ -1,5 +1,7 @@
 package com.zenkevich.nadin.tastyfood.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/base")
 public class BaseController {
+
+  private final static Logger logger = LoggerFactory.getLogger(BaseController.class);
+
   @RequestMapping(method = RequestMethod.GET)
   @ResponseBody
   public String test() {
+    logger.debug("Base Controller Test Method");
     return "Hello, World!";
   }
 }
